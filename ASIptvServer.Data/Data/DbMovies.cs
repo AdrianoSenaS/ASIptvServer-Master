ed13c64636798b26dc584fd3f9469ede5e3fde58
ADRIANO SENA <61697830+AdrianoSenaS@users.noreply.github.com>
2024-10-07 20:57:27 -0300

    Uptadte

--- ASIptvServer.Data/Data/DbMovies.cs
+++ ASIptvServer.Data/Data/DbMovies.cs
@@ -1,9 +1,53 @@
-﻿using System.Data.SQLite;
+﻿using System.Data;
+using System.Data.SQLite;
 
 namespace ASIptvServer.Data.Data
 {
     public  class DbMovies
     {
+
+        public static List<MovieModel> GetMovies()
+        {
+            List<MovieModel> movies = new List<MovieModel>();
+            
+            using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM MOVIES";
+                using (SQLiteCommand command= new SQLiteCommand(sql, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read()) 
+                        {
+                            movies.Add(new MovieModel(Convert.ToInt32(reader["ID"]), reader["TITLE"].ToString(), reader["LOGO"].ToString(), reader["CATEGORIES"].ToString(), reader["OVERVIEW"].ToString(), reader["url"].ToString()));
+                        }
+                    }
+                }
+            }
+            return movies;
+        }
+        public static List<MovieModel> GetMoviesId(int id)
+        {
+            List<MovieModel> movies = new List<MovieModel>();
+
+            using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM MOVIES WHRE ID="+id;
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            movies.Add(new MovieModel(Convert.ToInt32(reader["ID"]), reader["TITLE"].ToString(), reader["LOGO"].ToString(), reader["CATEGORIES"].ToString(), reader["OVERVIEW"].ToString(), reader["url"].ToString()));
+                        }
+                    }
+                }
+            }
+            return movies;
+        }
         public static void SetMovies(string Title, string Logo, string Categories, string Overview, string url)
         {
             using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
---
+++ ASIptvServer.Data/MovieModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASIptvServer.Data
+{
+    public class MovieModel
+    {
+        public MovieModel() { }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Logo { get; set; }
+        public string Categories { get; set; }
+
+        public string Overview { get; set; }
+        public string Url { get; set; }
+        public MovieModel(int id, string title, string logo, string categories, string overview, string url) 
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Logo = logo;
+            this.Categories = categories;
+            this.Overview = overview;
+            this.Url = url;
+        }
+
+
+    }
+}
