28bbe03747ce946e0767aea15cf9a5907e2f26b8
ADRIANO SENA <61697830+AdrianoSenaS@users.noreply.github.com>
2024-10-13 15:31:36 -0300

    Update DbMovies.cs

--- ASIptvServer.Data/Data/DbMovies.cs
+++ ASIptvServer.Data/Data/DbMovies.cs
@@ -7,176 +7,214 @@ namespace ASIptvServer.Data.Data
     {
         public static List<MovieModel> GetMovies()
         {
-            List<MovieModel> movies = new List<MovieModel>();
-            using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
+            try
             {
-                connection.Open();
-                string sql = "SELECT * FROM MOVIES";
-                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                List<MovieModel> movies = new List<MovieModel>();
+                using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
                 {
-                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    connection.Open();
+                    string sql = "SELECT * FROM MOVIES";
+                    using (SQLiteCommand command = new SQLiteCommand(sql, connection))
                     {
-                        while (reader.Read())
+                        using (SQLiteDataReader reader = command.ExecuteReader())
                         {
-                            movies.Add(new MovieModel()
+                            while (reader.Read())
                             {
-                                Id = reader.GetInt32(0),
-                                Title = reader.GetString(1),
-                                Logo = reader.GetString(2),
-                                Categories = reader.GetString(3),
-                                Overview = reader.GetString(4),
-                                Url = reader.GetString(5),
-                                Date = reader.GetString(6)
-                            });
+                                movies.Add(new MovieModel()
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Title = reader.GetString(1),
+                                    Logo = reader.GetString(2),
+                                    Categories = reader.GetString(3),
+                                    Overview = reader.GetString(4),
+                                    Url = reader.GetString(5),
+                                    Date = reader.GetString(6)
+                                });
+                            }
                         }
                     }
+                    connection.Close();
                 }
-                connection.Close();
+                return movies;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
-            return movies;
         }
         public static List<MovieModel> GetMoviesId(int id)
         {
-            List<MovieModel> movies = new List<MovieModel>();
-            using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
+            try
             {
-                connection.Open();
-                string sql = "SELECT * FROM MOVIES WHERE ID=@ID";
-                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                List<MovieModel> movies = new List<MovieModel>();
+                using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
                 {
-                    command.Parameters.AddWithValue("@ID", id);
-                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    connection.Open();
+                    string sql = "SELECT * FROM MOVIES WHERE ID=@ID";
+                    using (SQLiteCommand command = new SQLiteCommand(sql, connection))
                     {
-                        while (reader.Read())
+                        command.Parameters.AddWithValue("@ID", id);
+                        using (SQLiteDataReader reader = command.ExecuteReader())
                         {
-                            movies.Add(new MovieModel(){
-                               Id = reader.GetInt32(0),
-                               Title = reader.GetString(1),
-                               Logo = reader.GetString(2),
-                               Categories = reader.GetString(3),
-                               Overview = reader.GetString(4),
-                               Url = reader.GetString(5),
-                               Date = reader.GetString(6)
-                            });
+                            while (reader.Read())
+                            {
+                                movies.Add(new MovieModel()
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Title = reader.GetString(1),
+                                    Logo = reader.GetString(2),
+                                    Categories = reader.GetString(3),
+                                    Overview = reader.GetString(4),
+                                    Url = reader.GetString(5),
+                                    Date = reader.GetString(6)
+                                });
+                            }
                         }
                     }
+                    connection.Close();
                 }
-                connection.Close();
+                return movies;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
-            return movies;
         }
         public static void SetMovies(MovieModel movie)
         {
-            using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
+            try
             {
-                connection.Open();
-                string sql = "SELECT COUNT(1) FROM MOVIES WHERE TITLE = @TITLE";
-                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
                 {
-                    command.Parameters.AddWithValue("@TITLE", movie.Title);
-                    var count = Convert.ToInt32(command.ExecuteScalar());
-                    if (count == 0)
+                    connection.Open();
+                    string sql = "SELECT COUNT(1) FROM MOVIES WHERE TITLE = @TITLE";
+                    using (SQLiteCommand command = new SQLiteCommand(sql, connection))
                     {
-                        var sql1 = "INSERT INTO MOVIES (TITLE, LOGO, CATEGORIES, OVERVIEW, URL, DATE)VALUES(@TITLE, @LOGO, @CATEGORIES, @OVERVIEW, @URL, @DATE)";
-                        using (SQLiteCommand command1 = new SQLiteCommand(sql1, connection))
+                        command.Parameters.AddWithValue("@TITLE", movie.Title);
+                        var count = Convert.ToInt32(command.ExecuteScalar());
+                        if (count == 0)
                         {
-                            command1.Parameters.AddWithValue("@TITLE", movie.Title);
-                            command1.Parameters.AddWithValue("@LOGO", movie.Logo);
-                            command1.Parameters.AddWithValue("@CATEGORIES", movie.Categories.Replace("/", ""));
-                            command1.Parameters.AddWithValue("OVERVIEW", movie.Overview);
-                            command1.Parameters.AddWithValue("@URL", movie.Url);
-                            command1.Parameters.AddWithValue("@DATE", movie.Date);
-                            command1.ExecuteNonQuery();
+                            var sql1 = "INSERT INTO MOVIES (TITLE, LOGO, CATEGORIES, OVERVIEW, URL, DATE)VALUES(@TITLE, @LOGO, @CATEGORIES, @OVERVIEW, @URL, @DATE)";
+                            using (SQLiteCommand command1 = new SQLiteCommand(sql1, connection))
+                            {
+                                command1.Parameters.AddWithValue("@TITLE", movie.Title);
+                                command1.Parameters.AddWithValue("@LOGO", movie.Logo);
+                                command1.Parameters.AddWithValue("@CATEGORIES", movie.Categories.Replace("/", " "));
+                                command1.Parameters.AddWithValue("OVERVIEW", movie.Overview);
+                                command1.Parameters.AddWithValue("@URL", movie.Url);
+                                command1.Parameters.AddWithValue("@DATE", movie.Date);
+                                command1.ExecuteNonQuery();
+                            }
                         }
                     }
+                    connection.Close();
                 }
-                connection.Close();
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
         
         public static List<Categories> GetCategoryMovies()
         {
-            List<Categories> categories = new List<Categories>();
-            using(SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
+            try
             {
-                connection.Open();
-                string sql = "SELECT * FROM CATEGORIES";
-                using(SQLiteCommand command = new SQLiteCommand(sql, connection))
+                List<Categories> categories = new List<Categories>();
+                using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
                 {
-                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    connection.Open();
+                    string sql = "SELECT * FROM CATEGORIES";
+                    using (SQLiteCommand command = new SQLiteCommand(sql, connection))
                     {
-                        while (reader.Read())
+                        using (SQLiteDataReader reader = command.ExecuteReader())
                         {
-                            categories.Add(new Categories()
+                            while (reader.Read())
                             {
-                                Id = reader.GetInt32(0),
-                                Category = reader.GetString(1),
-                                SubCatagory = reader.GetString(2),
-                            });
+                                categories.Add(new Categories()
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Category = reader.GetString(1),
+                                    SubCatagory = reader.GetString(2),
+                                });
+                            }
                         }
                     }
+                    connection.Close();
                 }
-                connection.Close();
+                return categories;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
-            return categories;
         }
         public static List<MovieModel> GetCategoryMoviesId(string category)
         {
-            List<MovieModel> movies = new List<MovieModel>();
-            using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
+            try
             {
-                connection.Open();
-                string sql = "SELECT * FROM MOVIES WHERE CATEGORIES = @CATEGORIES ";
-                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                List<MovieModel> movies = new List<MovieModel>();
+                using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
                 {
-                    command.Parameters.AddWithValue("@CATEGORIES", category);
-                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    connection.Open();
+                    string sql = "SELECT * FROM MOVIES WHERE CATEGORIES = @CATEGORIES ";
+                    using (SQLiteCommand command = new SQLiteCommand(sql, connection))
                     {
-                        while (reader.Read())
+                        command.Parameters.AddWithValue("@CATEGORIES", category);
+                        using (SQLiteDataReader reader = command.ExecuteReader())
                         {
-                            movies.Add(new MovieModel()
+                            while (reader.Read())
                             {
-                                Id = reader.GetInt32(0),
-                                Title = reader.GetString(1),
-                                Logo = reader.GetString(2),
-                                Categories = reader.GetString(3),
-                                Overview = reader.GetString(4),
-                                Url = reader.GetString(5),
-                                Date = reader.GetString(6)
-                            });
+                                movies.Add(new MovieModel()
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Title = reader.GetString(1),
+                                    Logo = reader.GetString(2),
+                                    Categories = reader.GetString(3),
+                                    Overview = reader.GetString(4),
+                                    Url = reader.GetString(5),
+                                    Date = reader.GetString(6)
+                                });
+                            }
                         }
                     }
+                    connection.Close();
                 }
-                connection.Close();
+                return movies;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
-            return movies;
         }
         public static void SetCategoryMovies(Categories category)
         {
-            string sqls = "SELECT COUNT(1) FROM CATEGORIES WHERE CATEGORY = @CATEGORY";
-            using (SQLiteConnection connections = new SQLiteConnection(DbPath.Local))
+            try
             {
-                connections.Open();
-                using (SQLiteCommand commands = new SQLiteCommand(sqls, connections))
+                string sqls = "SELECT COUNT(1) FROM CATEGORIES WHERE CATEGORY = @CATEGORY";
+                using (SQLiteConnection connections = new SQLiteConnection(DbPath.Local))
                 {
-                    commands.Parameters.AddWithValue("@CATEGORY", category.Category.Replace("/", " "));
-                    var count = Convert.ToInt32(commands.ExecuteScalar());
-                    if (count == 0) 
+                    connections.Open();
+                    using (SQLiteCommand commands = new SQLiteCommand(sqls, connections))
                     {
-                        string sql = "INSERT INTO CATEGORIES (CATEGORY, SUBCATEGORY) VALUES(@CATEGORY, @SUBCATEGORY)";
-                        using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
+                        commands.Parameters.AddWithValue("@CATEGORY", category.Category.Replace("/", " "));
+                        var count = Convert.ToInt32(commands.ExecuteScalar());
+                        if (count == 0)
                         {
-                            connection.Open();
-                            using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                            string sql = "INSERT INTO CATEGORIES (CATEGORY, SUBCATEGORY) VALUES(@CATEGORY, @SUBCATEGORY)";
+                            using (SQLiteConnection connection = new SQLiteConnection(DbPath.Local))
                             {
-                                command.Parameters.AddWithValue("@CATEGORY", category.Category.Replace("/", " "));
-                                command.Parameters.AddWithValue("@SUBCATEGORY", "Movies");
-                                command.ExecuteNonQuery();
+                                connection.Open();
+                                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                                {
+                                    command.Parameters.AddWithValue("@CATEGORY", category.Category.Replace("/", " "));
+                                    command.Parameters.AddWithValue("@SUBCATEGORY", "Movies");
+                                    command.ExecuteNonQuery();
+                                }
+                                connection.Close();
                             }
-                            connection.Close();
                         }
                     }
-                }connections.Close();
+                    connections.Close();
+                }
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
     }
