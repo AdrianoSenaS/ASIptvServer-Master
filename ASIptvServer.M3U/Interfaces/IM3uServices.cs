be155c07a2e5875f19f104f510d817b0408e521c
ADRIANO SENA <61697830+AdrianoSenaS@users.noreply.github.com>
2025-01-26 18:34:27 -0300

    Create IM3uServices.cs

---
+++ ASIptvServer.M3U/Interfaces/IM3uServices.cs
@@ -0,0 +1,9 @@
+﻿using ASIptvServer.M3U.Model;
+
+namespace ASIptvServer.M3U.Interfaces
+{
+    public interface IM3uServices
+    {
+        public List<M3uModel> M3U(string[] lines, IVerificationStrings _verificationStrings)
+    }
+}
